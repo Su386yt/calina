3b76a182e7f36c1827b92859b24755ce2df08379
Su386 <83100266+Su386yt@users.noreply.github.com>
2025-02-12 14:36:13 -0500

    Documnetation

--- src/main/kotlin/dev/su386/calina/config/Config.kt
+++ src/main/kotlin/dev/su386/calina/config/Config.kt
@@ -62,6 +62,15 @@ open class Config(
 
     private val map: MutableMap<String, ConfigOption> = mutableMapOf()
 
+    /**
+     * Registers config option [option] at [path]
+     * Handles backwards compatibility
+     *
+     * @param path - Current path the option should be registered to
+     * @param option - Option to be registered
+     * @param previousPaths - Previous paths to be checked when loading config
+     * @throws IllegalArgumentException if path is duplicate
+     */
     fun register(path: String, option: ConfigOption, vararg previousPaths: String) {
         if(previousPaths.isNotEmpty()) {
             previousPathMap[path] = previousPaths as Array<String>
@@ -84,10 +93,24 @@ open class Config(
         }
     }
 
+    /**
+     * Registers config option [option] at path [path]
+     *
+     * @param path - Path of the config option the register
+     * @param option - Option to register at specified path
+     * @see dev.su386.calina.config.Config.register
+     */
     operator fun set(path: String, option: ConfigOption) {
         register(path, option)
     }
 
+    /**
+     * Accesses config option at specified path [path]
+     *
+     * @param path - Path to access config option
+     * @return Config option that has been registered at specified path
+     * @throws IllegalArgumentException if path is invalid
+     */
     operator fun get(path: String): ConfigOption {
         val trimmedPath = path.trim('/')
         if ("/" in trimmedPath) {
@@ -101,6 +124,15 @@ open class Config(
         }
     }
 
+    /**
+     * Gets option at [path] with specified type [T]
+     *
+     * @param path - Path to access config option
+     * @param T - type to cast
+     * @return Config option that has been registered at specified path
+     * @throws IllegalArgumentException if path is invalid
+     * @see dev.su386.calina.config.Config.get
+     */
     fun <T> get(path: String): T {
         return get(path).value()
     }
